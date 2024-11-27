0861883d83a98387ebe4b6718b7d4575cec362b2
HugodelaTorreGil <htorgil850@g.educaand.es>
2024-11-27 19:36:06 +0100

    Añado Random

---
+++ src/GIT/Random.java
@@ -0,0 +1,90 @@
+package GIT;
+
+public class Random{
+
+	public static void main(String[] args) {
+	
+    int[] numeros = new int[20];
+    Random random = new Random();
+    
+    for (int i = 0; i < 20; i++) {
+        
+    	numeros[i] = random.nextInt(10)+1;
+    	System.out.println((i+1) + " : " + numeros[i] + " ");
+    
+    	}
+	
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	private int nextInt(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
+
