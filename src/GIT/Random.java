a368c4066ed85ab92fa75230a123d2db37a5bdef
Elias <ewascal004@g.educaand.es>
2024-11-27 20:44:33 +0100

    cambio de creacion de numeros randoms de 30 a 15

--- src/GIT/Random.java
+++ src/GIT/Random.java
@@ -4,14 +4,14 @@ public class Random{
 
 	public static void main(String[] args) {
 	
-    int[] numeros = new int[30];
+    int[] numeros = new int[15];
     Random random = new Random();
     
-    for (int i = 0; i < 30; i++) {
+    for (int i = 0; i < numeros.length; i++) {
         
     	numeros[i] = random.nextInt(10)+1;
     	System.out.println((i+1) + " : " + numeros[i] + " ");
-    
+    	
     	}
 	
 	}
